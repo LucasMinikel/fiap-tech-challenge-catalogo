dde2f7acc79bdfc4e7f775df7b1d65ecd62acab4
Lucas Minikel <lucasminikel.m@gmail.com>
2024-12-02 11:24:10 -0300

    adicionando swagger

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 use DI\Bridge\Slim\Bridge;
 use DI\ContainerBuilder;
 use Dotenv\Dotenv;
-use Slim\Psr7\Request;
-use Slim\Psr7\Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -21,6 +19,41 @@ $app = Bridge::create($container);
 
 $app->addErrorMiddleware(true, true, true);
 $app->add(new \Slim\Middleware\BodyParsingMiddleware());
+
+$app->get('/openapi', function ($request, $response) {
+    $yamlFile = __DIR__ . '/../openapi.yaml';
+    $response->getBody()->write(file_get_contents($yamlFile));
+    return $response->withHeader('Content-Type', 'application/x-yaml');
+});
+
+$app->get('/docs', function ($request, $response) {
+    $html = <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>API Documentation</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+</head>
+<body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js" crossorigin></script>
+    <script>
+        window.onload = () => {
+            window.ui = SwaggerUIBundle({
+                url: '/openapi',
+                dom_id: '#swagger-ui',
+            });
+        };
+    </script>
+</body>
+</html>
+HTML;
+
+    $response->getBody()->write($html);
+    return $response->withHeader('Content-Type', 'text/html');
+});
 
 $app->post('/produtos', [\App\Infrastructure\API\Controllers\ProdutoController::class, 'criar']);
 $app->put('/produtos/{id}', [\App\Infrastructure\API\Controllers\ProdutoController::class, 'atualizar']);
